89274eaa28a01ef0bb63ecabc00e9a9c28f9d65f
Mathieu Coavoux <mathieu.coavoux@gmail.com>
2023-02-21 20:31:12 +0100

    Remove the comment and change the condition to execute the integration test

--- src/test/java/com/kamvity/samples/om/controller/OrderControllerTest.java
+++ src/test/java/com/kamvity/samples/om/controller/OrderControllerTest.java
@@ -3,16 +3,15 @@ package com.kamvity.samples.om.controller;
 import com.kamvity.samples.om.config.OrderManagementConfig;
 import com.kamvity.samples.om.response.OrderResponse;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -22,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(classes = OrderManagementConfig.class)
-@Profile("e2e")
+@EnabledIfEnvironmentVariable(named = "TEST_STAGE", matches = "integration")
 public class OrderControllerTest {
 
     @Value(value="${local.server.port}")
